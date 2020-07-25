7f45eebeefcf3f70bb36571d9d95ceb8cdbd032b
Niels Koster <n.koster@portavita.eu>
2020-07-25 17:48:04 +0200

    window now frame and fixed dimentions

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.on('ready', _ => {
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        height: 500, width: 300,
+        frame: false, resizable: false
     })
     mainWindow.loadURL(`file://${__dirname}/src/index.html`)
 })
