842d00b3bd1bcd857e6fb38d7e5e3ba016bc28b0
Niels Koster <n.koster@portavita.eu>
2020-07-26 18:40:03 +0200

    add tray stuff

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const electron = require('electron')
+const path = require('path')
 
-const { app, BrowserWindow } = electron
+const { app, BrowserWindow, Tray } = electron
 
-let mainWindow
+let mainWindow, tray
 
 app.on('ready', _ => {
     mainWindow = new BrowserWindow({
@@ -10,7 +11,19 @@ app.on('ready', _ => {
             nodeIntegration: true
         },
         height: 500, width: 300,
-        frame: false, resizable: false
+        frame: false, resizable: false, show: false
     })
     mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+    const iconName = process.platform === 'win32'
+        ? 'windows-icon.png'
+        : 'iconTemplate.png'
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
+    tray = new Tray(iconPath)
+    tray.on('click', _ => {
+        if (mainWindow.isVisible()) {
+            mainWindow.hide()
+        } else {
+            mainWindow.show()
+        }
+    })
 })
