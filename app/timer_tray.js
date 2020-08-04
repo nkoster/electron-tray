5aba07c06c9a5088d58f5059a6886c11215a1c88
Niels Koster <n.koster@portavita.eu>
2020-08-04 23:56:59 +0200

    right click menu in tray to quit - does not work in my mint linux

--- app/timer_tray.js
+++ app/timer_tray.js
@@ -1,12 +1,13 @@
 const electron = require('electron')
-const { Tray } = electron
+const { Tray, Menu, app } = electron
 
 class TimerTray extends Tray {
     constructor(iconPath, mainWindow) {
         super(iconPath)
         this.mainWindow = mainWindow
-        this.setToolTip('Drone Timer')
+        // this.setToolTip('Drone Timer')
         this.on('click', this.onClick.bind(this))
+        this.on('right-click', this.onRightClick.bind(this))
     }
     onClick(evt, bounds) {
         const { x, y } = bounds
@@ -24,6 +25,15 @@ class TimerTray extends Tray {
             })
             this.mainWindow.show()
         }
+    }
+    onRightClick() {
+        const menuConfig = Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                click: _ => app.quit()
+            }
+        ])
+        this.popUpContextMenu(menuConfig)
     }
 }
 
