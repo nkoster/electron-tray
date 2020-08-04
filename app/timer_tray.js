7e8732a96ec013fb5bdee8fdc700d3b4886d8af5
Niels Koster <n.koster@portavita.eu>
2020-08-04 23:13:31 +0200

    Refactor: mv event handler to sub class

--- app/timer_tray.js
+++ app/timer_tray.js
@@ -2,8 +2,27 @@ const electron = require('electron')
 const { Tray } = electron
 
 class TimerTray extends Tray {
-    constructor(iconPath) {
+    constructor(iconPath, mainWindow) {
         super(iconPath)
+        this.mainWindow = mainWindow
+        this.on('click', this.onClick.bind(this))
+    }
+    onClick(evt, bounds) {
+        const { x, y } = bounds
+        const { height, width } = this.mainWindow.getBounds()
+        console.log(x, y)
+        if (this.mainWindow.isVisible()) {
+            this.mainWindow.hide()
+        } else {
+            const yPos = process.platform === 'darwin'
+                ? y
+                : y - height
+            this.mainWindow.setBounds({
+                x: 1200, yPos,
+                height, width
+            })
+            this.mainWindow.show()
+        }
     }
 }
 
